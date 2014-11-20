7a6f7180d9fb248acea91b235a18694b0fe86eae
Alistair MacDonald <amacdonald@aisquared.com>
2014-11-19 23:43:45 -0500

    Doing something...

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-var something = 1;
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var server = connect().use(serveStatic(__dirname)).listen(8080);
+var io = require('socket.io')(server);
+
+
+
+
+io.on('connection', function (socket) {
+  socket.on('audio', function(data){
+    console.log(data);
+  })
+});
+
